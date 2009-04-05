c57e2dc6d543fcfd9580f1de398d403e9d6aa550
kaijenhsiao <kaijenhsiao@gmail.com>
2009-04-05 06:34:51 +0000

    finished transmission ratio calibration

---
+++ propagatewamconf.sh
@@ -0,0 +1,7 @@
+
+cp wam.conf ./BarrettWAM/btclient/wam.conf 
+cp wam.conf ./WAMinterface/WAMinterfacelib/testprograms/wam.conf 
+cp wam.conf ./WAMinterface/WAMinterfacelib/socketwamif/wam.conf 
+cp wam.conf ./WAMinterface/btdiag/wam.conf 
+cp wam.conf ./BarrettWAM/btclient/config/WAMlis.conf
+
