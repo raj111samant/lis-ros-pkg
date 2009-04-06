6ddeae2084dd9f6ff07d886cb9f7040cdc2f0bfc
kaijenhsiao <kaijenhsiao@gmail.com>
2009-04-06 22:12:58 +0000

    minor changes to gravityCompTestIK.py

--- WAMinterface/ROS/testprograms/gravityCompTestIK.py
+++ WAMinterface/ROS/testprograms/gravityCompTestIK.py
@@ -7,7 +7,7 @@ roslib.load_manifest('WAMinterface')
 
 from WAMClientROSFunctions import *
 
-#pretty-print a 4x4 matrix
+#pretty-print a 4x4 matrix in list form
 def ppmat4(mat):
     for i in range(4):
         print ' '.join(['%2.3f' % x for x in mat[i*4:(i+1)*4]])
@@ -16,6 +16,24 @@ def ppmat4(mat):
 #pretty-print a list of doubles to a string
 def pplisttostr(list):
     return ' '.join(['%2.3f' % x for x in list])
+
+#pretty-print a vector in nx1 scipy matrix form
+def ppscipyvecttostr(mat):
+    return ' '.join(['%2.3f' % x for x in mat.transpose().tolist()[0]])
+
+#create a 4x4 scipy matrix from list
+def mat4(list):
+    newmat = scipy.matrix(scipy.zeros([4,4],dtype=float))
+    for i in range(4):
+        for j in range(4):
+            newmat[i,j] = list[i*4+j]
+    return newmat
+
+#convert from joint0 frame to base frame
+joint0_to_base_mat = scipy.matrix([[1,0,0,.22],
+                                   [0,1,0,.14],
+                                   [0,0,1,.346],
+                                   [0,0,0,1]], dtype=float)
 
 
 print "starting the WAM client"
@@ -50,9 +68,10 @@ while(1):
     print "forward kinematics thinks:"
     palmrot = forward_kinematics(jointangles)
     ppmat4(palmrot)
-    tablepos = list(pos)
-    tablepos[2] += .346
-    print "position relative to table:", pplisttostr(tablepos)
+
+    scipypalmrot4 = mat4(palmrot)
+    basepalmmat = joint0_to_base_mat * scipypalmrot4
+    print "position relative to robot base:", ppscipyvecttostr(basepalmmat[0:3,3])
 
     print "analytical inverse kinematics came up with:"
     zeroangles = [0]*7
